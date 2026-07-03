b93e37a49a91c09db1cd8f3181820a87ce791683
JuancaSterba <sjcexe@gmail.com>
2026-07-03 06:44:56 -0300

    feat(core): agregar queries custom a AlumnoInscriptoRepository

--- backend/core/src/main/java/ar/edu/itec1misiones/repository/AlumnoInscriptoRepository.java
+++ backend/core/src/main/java/ar/edu/itec1misiones/repository/AlumnoInscriptoRepository.java
@@ -2,8 +2,13 @@ package ar.edu.itec1misiones.repository;
 
 import ar.edu.itec1misiones.model.AlumnoInscripto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlumnoInscriptoRepository extends JpaRepository<AlumnoInscripto, Long> {
+    boolean existsByAlumnoCarreraIdAndComisionId(Long alumnoCarreraId, Long comisionId);
+    long countByComisionId(Long comisionId);
+    List<AlumnoInscripto> findByAlumnoCarreraId(Long alumnoCarreraId);
 }
