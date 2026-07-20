a6f44ac2687bd033a803b670a9b57fe717a9fe77
JuancaSterba <sjcexe@gmail.com>
2026-07-19 23:59:09 -0300

    refactor(backend): eliminar entidad ModuloHorario obsoleto tras migración

--- backend/core/src/main/java/ar/edu/itec1misiones/exception/ModuloHorarioNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.itec1misiones.exception;
-
-public class ModuloHorarioNotFoundException extends RuntimeException {
-    public ModuloHorarioNotFoundException(Long id) {
-        super("Módulo horario no encontrado con id: " + id);
-    }
-}
--- backend/core/src/main/java/ar/edu/itec1misiones/repository/ModuloHorarioRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itec1misiones.repository;
-
-import ar.edu.itec1misiones.model.ModuloHorario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModuloHorarioRepository extends JpaRepository<ModuloHorario, Long> {
-}
